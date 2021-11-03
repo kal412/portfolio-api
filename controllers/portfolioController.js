ae467eec157fef07174fe96cfb41bf5ae666b86c
kal412 <devkota.kalyan98@gmail.com>
2021-11-03 22:28:48 +0545

    fix:apiKey

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -3,12 +3,9 @@ const Landing = require("../models/Landing");
 const About = require("../models/About");
 const Service = require("../models/Services");
 const Contact = require("../models/Contact");
-const ApiKey = require("../models/ApiKey");
-
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 
 exports.homepage = async (req, res) => {
+  // try {
   console.log("Accessing database");
   const landingData = await Landing.find({});
   const aboutData = await About.find({});
@@ -29,207 +26,93 @@ exports.homepage = async (req, res) => {
       ]);
     }
   });
+
+  //   console.log("Accessing database last")
+  // } catch (err) {
+  //   console.log("Error Occured ok")
+  //   res.status(500).send({ message: error.message || "Error Occured" })
+  // }
 };
 
 exports.updateService = async (req, res) => {
-  let isAuthorized = false;
+  const update = await Service.replaceOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      title: req.body.title,
+      image: req.body.image,
+      description: req.body.description,
+    }
+  );
 
-  ApiKey.find({}, (err, foundItem) => {
-    bcrypt.compare(
-      req.header("api-key"),
-      foundItem[0].apikey,
-      (error, result) => {
-        if (result) {
-          isAuthorized = true;
-        } else {
-          isAuthorized = false;
-        }
-      }
-    );
-  });
-
-  if (isAuthorized) {
-    const update = await Service.replaceOne(
-      {
-        _id: req.params.id,
-      },
-      {
-        title: req.body.title,
-        image: req.body.image,
-        description: req.body.description,
-      }
-    );
-
-    res.send(update);
-  } else {
-    res.status(403).send("You aren;t authorized to access.");
-  }
+  res.send(update);
 };
 
 exports.updateLanding = async (req, res) => {
-  let isAuthorized = false;
+  // res.send(req.body)
 
-  ApiKey.find({}, (err, foundItem) => {
-    bcrypt.compare(
-      req.header("api-key"),
-      foundItem[0].apikey,
-      (error, result) => {
-        if (result) {
-          isAuthorized = true;
-        } else {
-          isAuthorized = false;
-        }
-      }
-    );
-  });
+  const updateLandings = await Landing.replaceOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      title: req.body.title,
+      image: req.body.image,
+      name: req.body.name,
+    }
+  );
 
-  if (isAuthorized) {
-    const updateLandings = await Landing.replaceOne(
-      {
-        _id: req.params.id,
-      },
-      {
-        title: req.body.title,
-        image: req.body.image,
-        name: req.body.name,
-      }
-    );
-
-    res.send(updateLandings);
-  } else {
-    res.status(403).send("You aren;t authorized to access.");
-  }
+  res.send(updateLandings);
 };
 
 exports.updateAboutUs = async (req, res) => {
-  let isAuthorized = false;
+  const updateAbout = await About.replaceOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      title: req.body.title,
+      link: req.body.link,
+      description: req.body.description,
+    }
+  );
 
-  ApiKey.find({}, (err, foundItem) => {
-    bcrypt.compare(
-      req.header("api-key"),
-      foundItem[0].apikey,
-      (error, result) => {
-        if (result) {
-          isAuthorized = true;
-        } else {
-          isAuthorized = false;
-        }
-      }
-    );
-  });
-
-  if (isAuthorized) {
-    const updateAbout = await About.replaceOne(
-      {
-        _id: req.params.id,
-      },
-      {
-        title: req.body.title,
-        link: req.body.link,
-        description: req.body.description,
-      }
-    );
-
-    res.send(updateAbout);
-  } else {
-    res.status(403).send("You aren;t authorized to access.");
-  }
+  res.send(updateAbout);
 };
 
 exports.ContactUs = async (req, res) => {
-  let isAuthorized = false;
-
-  ApiKey.find({}, (err, foundItem) => {
-    bcrypt.compare(
-      req.header("api-key"),
-      foundItem[0].apikey,
-      (error, result) => {
-        if (result) {
-          isAuthorized = true;
-        } else {
-          isAuthorized = false;
-        }
-      }
-    );
+  const contact = new Contact({
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email,
+    message: req.body.message,
   });
 
-  if (isAuthorized) {
-    const contact = new Contact({
-      name: req.body.name,
-      phone: req.body.phone,
-      email: req.body.email,
-      message: req.body.message,
-    });
-
-    contact.save((err) => {
-      if (err) {
-        res.status(200).send("Messaged saved successfully.");
-      } else {
-        res.status(err.status).send(err.message);
-      }
-    });
-  } else {
-    res.status(403).send("You aren;t authorized to access.");
-  }
+  contact.save((err) => {
+    if (err) {
+      res.status(200).send("Messaged saved successfully.");
+    } else {
+      res.status(err.status).send(err.message);
+    }
+  });
 };
 
 exports.getContactUs = async (req, res) => {
-  let isAuthorized = false;
-
-  ApiKey.find({}, (err, foundItem) => {
-    bcrypt.compare(
-      req.header("api-key"),
-      foundItem[0].apikey,
-      (error, result) => {
-        if (result) {
-          isAuthorized = true;
-        } else {
-          isAuthorized = false;
-        }
-      }
-    );
+  console.log("Accessing database");
+  await Contact.find({}, (err, foundItems) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Inside Database");
+      res.send(foundItems);
+    }
   });
-
-  if (isAuthorized) {
-    console.log("Accessing database");
-    await Contact.find({}, (err, foundItems) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Inside Database");
-        res.send(foundItems);
-      }
-    });
-  } else {
-    res.status(403).send("You aren;t authorized to access.");
-  }
 };
 
 exports.deleteContactUs = async (req, res) => {
-  let isAuthorized = false;
+  console.log("Accessing database");
+  const deleteContact = await Contact.deleteOne({ _id: req.params.id });
 
-  ApiKey.find({}, (err, foundItem) => {
-    bcrypt.compare(
-      req.header("api-key"),
-      foundItem[0].apikey,
-      (error, result) => {
-        if (result) {
-          isAuthorized = true;
-        } else {
-          isAuthorized = false;
-        }
-      }
-    );
-  });
-
-  if (isAuthorized) {
-    console.log("Accessing database");
-    const deleteContact = await Contact.deleteOne({
-      _id: req.params.id,
-    });
-
-    res.send(deleteContact);
-  } else {
-    res.status(403).send("You aren;t authorized to access.");
-  }
+  res.send(deleteContact);
 };
